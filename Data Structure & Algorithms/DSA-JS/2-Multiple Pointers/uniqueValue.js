20838c2bd9ca6d996d6c2ecb77b19063a55a0b8c
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2023-12-03 20:06:34 +0100

    Multiple Pointers: Added alternate approach to solve the problem without modifying the existing array

--- Data Structure & Algorithms/DSA-JS/2-Multiple Pointers/uniqueValue.js
+++ Data Structure & Algorithms/DSA-JS/2-Multiple Pointers/uniqueValue.js
@@ -5,19 +5,32 @@ Question:
 
 Sample Inputs:
     countUnique[1,1,2,4,4,5] => 4
-*/ 
+*/
 
-
-const countUnique = arr => {
-    if (arr.length === 0) return 0;
-  let i = 0;
+const countUnique = (arr) => {
+  if (arr.length === 0) return 0
+  let i = 0
   for (let j = 1; j < arr.length; j++) {
-      if (arr[i] !== arr[j]) {
-          i++;
-          arr[i] = arr[j];
-      }
+    if (arr[i] !== arr[j]) {
+      i++
+      arr[i] = arr[j]
+    }
   }
-  return i+1;
+  return i + 1
 }
 
-console.log(uniqueArray([1,2,2,3,3,4,4]));
+console.log(countUnique([1, 2, 2, 3, 3, 4, 4]))
+
+// Alternate approach without modifying the existing array
+
+const countUnique2 = (arr) => {
+  if (arr.length === 0) return 0
+  let [i, uniqueCount] = [0, 1]
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      uniqueCount++
+      i = j
+    }
+  }
+  return uniqueCount
+}
