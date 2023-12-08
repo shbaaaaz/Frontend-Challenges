dcf63b453a77456fcca26e4a303a973ed017bfe7
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2023-12-08 16:23:15 +0100

    fix + alternate approach for searchNumInArray

--- Data Structure & Algorithms/DSA-JS/4-Divide and Conquer/searchNumInArray.js
+++ Data Structure & Algorithms/DSA-JS/4-Divide and Conquer/searchNumInArray.js
@@ -7,17 +7,27 @@ Sample Input:
     searchNumberInArray([1,2,3,4,5,6,7,8,9,10,11,12], 5)
 */
 
-const searchNumberInArray = (arr, num) => {
-    if (arr.length === 0) return false;
-    const sortedArr = arr.sort((a, b) => a-b);
-    let [min, max] = [0, sortedArr.length-1];
-    if (num > sortedArr[max]) return false;
-    while (min <= max) {
-        let middle = Math.floor((min+max) / 2);
-        let value = sortedArr[middle];
-        if (value < num) min = middle + 1;
-        else if (value > num) max = middle - 1;
-        else return middle;
-    }
-    return false;   
+// Normal Approach using for loop
+
+const searchNumberInArray_1 = (arr, num) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) return i
+  }
+  return false
+}
+
+// If the array is sorted, Divide and conquer
+
+const searchNumberInArray = (sortedArr, num) => {
+  if (sortedArr.length === 0) return false
+  let [min, max] = [0, sortedArr.length - 1]
+  if (num > sortedArr[max]) return false
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2)
+    let value = sortedArr[middle]
+    if (value < num) min = middle + 1
+    else if (value > num) max = middle - 1
+    else return middle
+  }
+  return false
 }
