0fb5716f2cf291a8f89fb97ef2861c246388d7a3
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2024-01-21 14:15:25 +0100

    Optimized duplicateCheck solution

--- Data Structure & Algorithms/DSA-JS/1-Frequency Counter/duplicateCheck.js
+++ Data Structure & Algorithms/DSA-JS/1-Frequency Counter/duplicateCheck.js
@@ -9,18 +9,14 @@ Sample Inputs:
     areThereDuplicates('a', 'b', 'c', 'a') // true
 */
 
-const objectCreator = (arr) => {
-    return arr.reduce((acc, curr) => {
-        acc[curr] = (acc[curr] || 0) + 1;
-        return acc;
-    }, {})
-}
-
 const areThereDuplicates = (...args) => {
-    if (args.length === 0) return false;
-    const frequencyCounter = objectCreator(args);
-    for (let key in frequencyCounter) {
-        if (frequencyCounter[key] > 1) return true;
-    }
-    return false;
+  if (args.length === 0) return
+
+  const frequencyCounter = {}
+
+  for (let val of args) {
+    if (frequencyCounter[val]) return true
+    else frequencyCounter[val] = 1
+  }
+  return false
 }
